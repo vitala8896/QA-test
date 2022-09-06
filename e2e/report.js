90ba9411ddbc219d5d9716331e3cbd61ff029e4e
Vitaliy Dyachuk <vitala8896@gmail.com>
2022-09-06 23:24:44 +0300

    fix: changed folder report

--- e2e/report.js
+++ e2e/report.js
@@ -1,8 +1,8 @@
 const reporter = require("cucumber-html-reporter")
 const options ={
      theme:'bootstrap',
-     jsonFile:'report/report.json',
-     output:'report/cucumber-html-report.html',
+     jsonFile:'report.json',
+     output:'cucumber-html-report.html',
      reportSuiteAsScenaros:true,
      launchReport:false,
 }
